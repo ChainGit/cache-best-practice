b59cfcfa27372114bd646769e40b11ee4c05208a
bangquan.qian <chainz@foxmail.com>
2020-08-17 17:36:23 +0800

    commit

---
+++ src/main/java/com/opensource/cache/DataObject2Controller.java
@@ -0,0 +1,17 @@
+package com.opensource.cache;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author bangquan.qian
+ * @date 2020-08-16 12:39
+ */
+@Slf4j
+@RestController
+@RequestMapping("/data2")
+public class DataObject2Controller {
+
+
+}
---
+++ src/main/java/com/opensource/cache/DataObject2Service.java
@@ -0,0 +1,13 @@
+package com.opensource.cache;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author bangquan.qian
+ * @date 2020-08-16 12:40
+ */
+@Service
+public class DataObject2Service {
+
+
+}
